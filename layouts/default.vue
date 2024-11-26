4dbf0d029857e75b09e72954fa68369c78c0554a
uhkfc <daniel.dietzler@student.kit.edu>
2024-11-26 15:31:20 +0100

    chore: footer styling

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,9 +4,9 @@
   </div>
   <footer
     v-if="$slidev.nav.currentLayout !== 'cover'"
-    class="absolute flex bottom-0 w-full m-4 justify-between pr-10"
+    class="absolute flex bottom-0 w-full m-4 justify-between pr-10 font-light text-sm"
   >
     <div>Daniel Dietzler</div>
-    <div>{{ $slidev.nav.currentPage }}/{{ $slidev.nav.total }}</div>
+    <div class="font-normal">{{ $slidev.nav.currentPage }}/{{ $slidev.nav.total }}</div>
   </footer>
 </template>
